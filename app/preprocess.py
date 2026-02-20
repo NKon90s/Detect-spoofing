c57f83f2158a8c75fe42af57ea1f55b7f3c83c58
Konrad <nkonrad93@gmail.com>
2026-02-20 19:43:27 +0100

    preprocess function created

---
+++ app/preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import joblib
+ 
+
+# Add a few extra parameters that helps classification
+def add_extra_features(df):
+    df['doppler_prrate_nonlinear'] = (df['doppler'] - df['pr_rate'])**2
+    df['snr_z'] = (df['snr'] - df['snr_mean_5']) / (df['snr_std_5'] + 1e-6)
+    return df
+
+
+def preProcess(df):
+    df = df.copy()
+
+    # handling time
+    if "time" in df.columns:
+        df["time"] = pd.to_datetime(df["time"])
+
+    # add_extra_features
+    df = add_extra_features(df)
+
+    #loading label encoders:
+    df["sys"] = df["sys"].map({"G": 0, "R": 1, "E": 2})
+
+    # Fill missing
+    df = df.fillna(-9999)
+
+    drop_cols = ["time", "attack_label", "attack_type", "time_utc", "sv", "prn"]
+    X = df.drop(columns=drop_cols, errors="ignore")
+
+    return X, df
